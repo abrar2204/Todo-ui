09582d3183dcf8c0c924c18f47d74384d8162f9f
Abrar Hasan <abrar.hasan@thoughtworks.com>
2022-06-30 14:22:21 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: path.resolve(__dirname,"src/index.js")
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname,"dist")
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname,"public/index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot:true,
+    compress: true,
+    port: 3000,
+  },
+}
